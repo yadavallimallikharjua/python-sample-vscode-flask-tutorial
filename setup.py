7094a13962e46e80c083597d137d6fd3f962dabd
yadavallimallikharjua <95014424+yadavallimallikharjua@users.noreply.github.com>
2022-06-03 20:14:16 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+  #!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='jfrog-python',
+    version='1.0',
+    description='building Python project with JFrog products',
+    author='yadavallimallikharjuna',
+    author_email='yadavallimallikharjuna@gmail.com',
+    url='https://github.com/yadavallimallikharjua/python-sample-vscode-flask-tutorial.git',
+    packages=['pythonTest'],
+    install_requires=['PyYAML>3.11', 'nltk'],
+)
+
+
